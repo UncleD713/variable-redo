825ab1b923ccc9b262e5b577a258d28ef2717d67
Derrick Bell <dbell34@mail.valenciacollege.edu>
2021-03-07 10:47:32 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Declare three variables firstName, lastName, and age
+var firstName;
+    firstName="Derrick";
+var lastName;
+    lastName="Bell";
+var numberAge;
+    numberAge="52";
+// Fire an alert with the words below
+alert(" My name is "  +  firstName );
+alert(" I am "  +  numberAge  + " years old" );
+
+ // Log a message saying how much fun I'm having in class
+console.log("Having fun and stuyding hard in class"); 
